667cdc09fb5c6cf9480695df4ee9153a2fc47012
DmitrySergienko <dmitrisergienko@gmail.com>
2022-07-17 16:29:12 +0400

    10. WeatherRepo

---
+++ app/src/main/java/ru/ds/weatherfirst/data/repository/WeatherRepo.kt
@@ -0,0 +1,15 @@
+package ru.ds.weatherfirst.data.repository
+
+import ru.ds.weatherfirst.data.api.WeatherApi
+import ru.ds.weatherfirst.data.api.model.Weather
+import javax.inject.Inject
+
+private const val API_KEY = "886e042c31bc49c3a3f131017220902"
+
+class WeatherRepo @Inject constructor(
+    private val weatherApi: WeatherApi
+) {
+    suspend fun weather(): List<Weather>{
+        return weatherApi.getWeather(API_KEY,"London",3)
+    }
+}
