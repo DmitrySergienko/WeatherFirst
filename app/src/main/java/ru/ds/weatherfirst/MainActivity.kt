45d527f55f4f49e0ca3ad3dc9b3d6b64e041cddf
DmitrySergienko <dmitrisergienko@gmail.com>
2023-03-16 10:52:26 +0300

    101. Add permissions

--- app/src/main/java/ru/ds/weatherfirst/MainActivity.kt
+++ app/src/main/java/ru/ds/weatherfirst/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.ds.weatherfirst
 
+//import androidx.activity.result.contract.ActivityResultContracts
+import android.Manifest
 import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -20,14 +23,17 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.Navigation
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import dagger.hilt.android.AndroidEntryPoint
 import ru.ds.weatherfirst.domain.connectivity.ConnectivityObserver
 import ru.ds.weatherfirst.domain.connectivity.NetworkConnectivityObserver
+import ru.ds.weatherfirst.presentation.permission.RequestMultiplePermissions
 import ru.ds.weatherfirst.presentation.screens.HomeViewModel
 import ru.ds.weatherfirst.presentation.theme.WeatherFirstTheme
 import ru.ds.weatherfirst.ui.SetupNavGraph
 
 @AndroidEntryPoint
+@ExperimentalPermissionsApi
 class MainActivity : ComponentActivity() {
 
     //connectivity observer
@@ -36,6 +42,7 @@ class MainActivity : ComponentActivity() {
     //navigation between screens
     lateinit var navController: NavHostController
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -59,6 +66,15 @@ class MainActivity : ComponentActivity() {
         connectivityObserver = NetworkConnectivityObserver(applicationContext)
 
         setContent {
+            //request permissions
+            RequestMultiplePermissions(
+                permissions = listOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.POST_NOTIFICATIONS,
+
+                )
+            )
 
             //connectivity observer
             val status by connectivityObserver.observe().collectAsState(
--- app/src/main/java/ru/ds/weatherfirst/data/location/DefaultLocationTracker.kt
+++ app/src/main/java/ru/ds/weatherfirst/data/location/DefaultLocationTracker.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -20,28 +22,27 @@ class DefaultLocationTracker @Inject constructor(
     private val application: Application
 ): LocationTracker {
 
-    //get location
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override suspend fun getCurrentLocation(): Location? {
-        val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
-            application,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-        val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
-            application,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
+        //
+        val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(application,
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(application,
+            Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
         val locationManager = application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
         val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        if (!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
+        if(!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
             return null
         }
 
         return suspendCancellableCoroutine { cont ->
             locationClient.lastLocation.apply {
-                if (isComplete) {
-                    if (isSuccessful) {
+                if(isComplete) {
+                    if(isSuccessful) {
                         cont.resume(result)
                     } else {
                         cont.resume(null)
